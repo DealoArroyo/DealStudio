69d6698b923c422b4e4648833307ebc3b7a81c09
Dealo Arroyo <dealoarroyo@Mac-mini-de-Dealo.local>
2025-08-23 17:20:07 -0600

    Cambio de Descripción de contenido y adición de plugin sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output : "static",
+  site: "https://dealstudio.com.mx",
+  integrations: [sitemap()],
 
   vite:{
     plugins: [
